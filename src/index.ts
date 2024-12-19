9d5896bca718575456905b6b688b5370b94643c2
emmanuel.lima <86535005+Emmanuel194@users.noreply.github.com>
2024-12-19 16:43:26 -0300

    implementation of the nearby markets API and change in the dashboard layout

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes";
 import listRoutes from "./routes/listRoutes";
 import dotenv from "dotenv";
-// import { authenticateToken } from "./middlewares/authMiddleware";
-// mport { validateSignup } from "./middlewares/validationMiddleware";
+import axios from "axios";
 
 dotenv.config();
 
@@ -15,8 +14,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/auth", authRoutes);
-// app.use(authenticateToken);
 app.use("/api", listRoutes);
+
+const apiKey = process.env.GOOGLE_API_KEY;
+
+app.get("/api/nearby-markets", async (req, res) => {
+  const { latitude, longitude } = req.query;
+  console.log(`Recebido: latitude=${latitude}, longitude=${longitude}`);
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=supermarket&key=${apiKey}`;
+  console.log("URL da Requisição:", url);
+  try {
+    const response = await axios.get(url);
+    console.log("Resposta da API:", response.data);
+    res.json(response.data.results);
+  } catch (error) {
+    console.error("Erro ao buscar mercados próximos:", error);
+    res.status(500).send("Erro ao buscar mercados próximos");
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
